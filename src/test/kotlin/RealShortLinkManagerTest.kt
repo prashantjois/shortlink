cb57f3433e3edf5f756bc853532e6ac2263ac850
Prashant Jois <github@jois.ca>
2023-12-22 19:06:37 -0800

    remove test code

--- src/test/kotlin/RealShortLinkManagerTest.kt
+++
@@ -1,9 +0,0 @@
-import org.junit.jupiter.api.Assertions.*
-import org.junit.jupiter.api.Test
-
-class RealShortLinkManagerTest {
-  @Test
-  fun stuff() {
-    print("${Long.MAX_VALUE} -- \n")
-  }
-}
